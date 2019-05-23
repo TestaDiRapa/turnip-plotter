abedf9f8eed4ee6dd3da717ccd246be79c93924d
TestaDiRapa <v.c.pierro@gmail.com>
2019-05-23 16:38:19 +0200

    Refactored first version

---
+++ device/__init__.py
@@ -0,0 +1,34 @@
+import re
+import serial
+
+
+def initialize_serial(port):
+    """
+    Check if the device is available and if the data are correctly formatted
+    :param port: the port to listen
+    :return: the number of values to plot
+    """
+    with serial.Serial(port) as device:
+        for i in range(100):
+            line = device.readline().decode('utf-8')
+            if re.search(r'^-?\d+(\.\d+)?(\t-?\d+(\.\d+)?)+\r?\n$', line):
+                break
+        else:
+            raise Exception("Wrong data format!")
+
+        return len(line.split('\t'))
+
+
+def read_from_serial(buffers):
+    with serial.Serial('/dev/ttyACM0') as device:
+        while not re.search(r'^-?\d+(\.\d+)?(\t-?\d+(\.\d+)?)+\r?\n$', device.readline().decode('utf-8')):
+            pass
+        while True:
+            line = re.sub('\r', '', re.sub('\n', '', device.readline().decode('utf-8')))
+            try:
+                values = [int(x) for x in line.split('\t')]
+                for j in range(len(buffers)):
+                    buffers[j].pop(0)
+                    buffers[j].append(values[j])
+            except ValueError:
+                pass
---
+++ main.py
@@ -0,0 +1,49 @@
+from threading import Thread
+import device
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+
+
+colors = ['red', 'green', 'blue', 'yellow', 'black']
+
+
+def real_time_plot(fig, buffers, sub_plots):
+    for i in range(len(buffers)):
+        sub_plots[i].clear()
+        sub_plots[i].plot(buffers[i], color=colors[i % len(colors)])
+
+
+def main():
+
+    # Reads from the user the serial port to read from
+    port = input("Insert the serial directory [/dev/ttyACM0]: ")
+    if len(port) == 0:
+        port = "/dev/ttyACM0"
+
+    length = input("Specify the x-axis length [100]: ")
+    try:
+        length = int(length)
+        if length < 0:
+            length = 100
+    except ValueError:
+        length = 100
+
+    num_values = device.initialize_serial(port)
+
+    buffers = []
+    for i in range(num_values):
+        buffers.append([0 for i in range(length)])
+
+    fig = plt.figure()
+
+    sub_plots = []
+    for i in range(num_values):
+        sub_plots.append(fig.add_subplot(num_values+1, 1, i+1))
+
+    Thread(target=device.read_from_serial, args=(buffers,)).start()
+    a = animation.FuncAnimation(fig, real_time_plot, fargs=(buffers, sub_plots), interval=10)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
